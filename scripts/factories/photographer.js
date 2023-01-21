64a9905e8abdd8355a2c2649f42c1138a93a51c4
Pasquinucci Sandrine <sandrine.pasquinucci@gmail.com>
2023-01-21 09:37:33 +0100

    Etape 1+2+3, manque css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,63 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
 
+function photographerFactory(data) {
+    //existant mis en commentaire
+   // const { name, portrait } = data;
+   const { portrait,name, id, city, country, price,  tagline } = data;
+   console.log(data);
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const a = document.createElement('a');
+        const Artname = document.createElement( 'h2' ); 
+        const Artcity = document.createElement( 'div' );
+        const Artcountry = document.createElement( 'div' );
+        const Artprice = document.createElement( 'div' );
+        const Arttagline = document.createElement( 'div' );
+        
+   
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        Artname.textContent = name;
+        Artcity.textContent = city;
+        Artcountry.textContent = city + ", " +country;
+        Artprice.textContent =price + "€/jour";
+        Arttagline.textContent = tagline;
+        
+
+
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(Artname);
+        
+        article.appendChild(Artcountry);
+        article.appendChild(Arttagline);
+        article.appendChild(Artprice);
+       
+        
+        
+        
         return (article);
+      
     }
-    return { name, picture, getUserCardDOM }
+    
+    return { portrait,name, id, country, price,  tagline ,
+        getUserCardDOM} 
 }
+
+// function photographerFactory(data) {
+//     const { name, portrait } = data;
+
+//     const picture = `assets/photographers/${portrait}`;
+
+//     function getUserCardDOM() {
+//         const article = document.createElement( 'article' );
+//         const img = document.createElement( 'img' );
+//         //img.setAttribute("src", picture)
+//         const h2 = document.createElement( 'h2' );
+//         h2.textContent = name;
+//         article.appendChild(img);
+//         article.appendChild(h2);
+//         return (article);
+//     }
+//     return { name, picture, getUserCardDOM }
+// }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,109 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
 
+   async function getPhotographers() {
+//         //je charge les données du fichier photographers.json je les stocke dans photographers 
+
+        // fetch('./data/photographers.json')
+        //  .then(reponse=>reponse.json())
+        //  .then ((reponse1) => {
+   
+        //     const photographers = reponse1.photographers;  
+        //      console.log(photographers);
+            
+           
+        
+            
+        //   });
+        //   return {photographers: [...photographers]} 
+     
+      
+
+//      };
+// async function fetchPhotographersJSON() {
+// const response = await fetch("./data/photographers.json");
+// const reponse1 = await response.json();
+// return reponse1;
+// }
+// fetchPhotographersJSON.then(reponse1 => {
+//     const photographers = reponse1.photographers;  
+// });
+//    return {
+//                   photographers: [...photographers]
+                
+//                   }
+                
+//}
+//async function getPhotographers() {
+    // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+    // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+    
+    // marche pas
+    //         fetch("./data/photographers.json")
+    //      .then(reponse=>reponse.json())
+    //      .then ((reponse2) => {
+    //         console.log(reponse2);
+    //         let photographers = reponse2.photographers;
+    //         console.log(photographers);
+    //         // return {
+    //         //     photographers: [...photographers]
+    //         //     }
+        
+            
+    //      });
+
+    //  return {
+    //              photographers: [...photographers]
+    //              }
+
+    const url = './data/photographers.json';
+    const requete = await fetch(url, {
+        method: 'GET'
+    });
+
+  
+        let data = await requete.json();
+        const photographers = data.photographers;
+        // console.log(photographers)
+    
+        
+    
+    
+     return {
+         photographers: [...photographers]
+   
+         }
+//exemple fourni par ocr
+    // let photographers = [
+    //     {
+    //         "name": "Ma data test",
+    //         "id": 1,
+    //         "city": "Paris",
+    //         "country": "France",
+    //         "tagline": "Ceci est ma data test",
+    //         "price": 400,
+    //         "portrait": "account.png"
+    //     },
+    //     {
+    //         "name": "Autre data test",
+    //         "id": 2,
+    //         "city": "Londres",
+    //         "country": "UK",
+    //         "tagline": "Ceci est ma data test 2",
+    //         "price": 500,
+    //         "portrait": "account.png"
+    //     },
+    // ]
+    // et bien retourner le tableau photographers seulement une fois récupéré
+    //return ({
+    //    photographers: [...photographers, ...photographers, ...photographers]})
+}
+
+
+
+
+// fonctions photographerFactory et getUserCardDom sont décrite dans ./scripts/factories/photographer.js
+// fonctions qui permettent de dispatcher les infos des 6 lignes du fichier photographers
+// une ligne par photographe
+//et de l'afficher dans le dom
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
@@ -38,9 +116,14 @@
 
     async function init() {
         // Récupère les datas des photographes
+        //const photographers await (attend le résultat de la fonction getphotographer definie plus haut)
         const { photographers } = await getPhotographers();
+        console.log(photographers);
+        //fonction displaydata est defini plus haut 
         displayData(photographers);
+        console.log(photographers);
     };
-    
+
+    //initialisation de la page index et affchage des photographes présent dans le fichier json
     init();
     
